8b0ed06e87cf9ccc421e97d99af324781e30b9bd
Mariia Verhun <mariia.verhun@gmail.com>
2024-05-30 15:02:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def calculate_stats(module):
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    with open("module.py", 'r') as f:
+        calculate_stats(f)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
